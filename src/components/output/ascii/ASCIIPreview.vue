246fbaee26628b40a6c1971ce81aded12976ecb7
Milorad Filipovic <milorad.filipovic19@gmail.com>
2025-08-31 20:22:05 +0200

    🔨 Optmizing tree css

--- src/components/output/ascii/ASCIIPreview.vue
+++ src/components/output/ascii/ASCIIPreview.vue
@@ -57,18 +57,18 @@ const htmlResult = computed(() => htmlGenerator.renderTree(props.tree, currentTh
 .interactive-ascii-tree {
   --background-color: #ffffff;
   --background-muted: #eeeeee;
-  --text-color-base: #222222;
+  --text-color: #222222;
   --text-color-muted: #999999;
 
   &[data-theme='dark'] {
     --background-color: #222222;
     --background-muted: #555555;
-    --text-color-base: #ffffff;
+    --text-color: #ffffff;
     --text-color-muted: #aaaaaa;
   }
 
   --indent: 4ch;
-  --v-line: '│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A';
+  --v-line: '│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A│\A';
 
   list-style: none;
   display: flex;
@@ -79,85 +79,83 @@ const htmlResult = computed(() => htmlGenerator.renderTree(props.tree, currentTh
   font-size: 16px;
   line-height: 1;
   font-family: "Consolas", "Menlo", "DejaVu Sans Mono", "Lucida Console", monospace;
-  color: var(--text-color-base);
+  color: var(--text-color);
   background-color: var(--background-color);
 
-  ul {
+  ul,
+  li {
     list-style: none;
     margin: 0;
     padding: 0;
   }
-  
+
   li {
-    margin: 0;
-    padding: 0;
     padding-top: 6px;
     position: relative;
-    
-    li {
-      padding-left: var(--indent);
-      
-      > *:first-child::before {
-        content: '├── ';
-        color: var(--text-color-muted);
-        position: absolute;
-        left: 0;
-      }
-      
-      &:last-child > *:first-child::before {
-        content: '└── ';
-      }
-      
-      &::after {
-        content: var(--v-line);
-        color: var(--text-color-muted);
-        position: absolute;
-        left: 0;
-        top: 1.4em;
-        white-space: pre-line;
-        line-height: 1;
-        pointer-events: none;
-        overflow: hidden;
-        height: calc(100% - 1.4em);
-      }
-      
-      &:last-child::after {
-        display: none;
-      }
-    }
   }
-  
+
+  li li {
+    padding-left: var(--indent);
+  }
+
+  li li > *:first-child::before {
+    content: '├── ';
+    color: var(--text-color-muted);
+    position: absolute;
+    left: 0;
+  }
+
+  li li:last-child > *:first-child::before {
+    content: '└── ';
+  }
+
+  li li::after {
+    content: var(--v-line);
+    color: var(--text-color-muted);
+    position: absolute;
+    left: 0;
+    top: 1.4em;
+    white-space: pre-line;
+    line-height: 1;
+    pointer-events: none;
+    overflow: hidden;
+    height: calc(100% - 1.4em);
+  }
+
+  li li:last-child::after {
+    display: none;
+  }
+
   > li {
     &::after,
     > *:first-child::before {
       display: none !important;
     }
   }
-  
+
   summary {
     cursor: pointer;
-    display: inline;
     outline: none;
     user-select: none;
-    
-    &::marker,
-    &::-webkit-details-marker {
-      display: none;
-    }
-    
-    &::after {
-      content: '[+]';
-      color: var(--text-color-muted);
-    }
-    
-    details[open] > &::after {
-      content: '[-]';
-    }
-    
-    &:hover {
-      background: var(--background-muted);
-      border-radius: 2px;
-    }
+  }
+
+  summary::marker,
+  summary::-webkit-details-marker {
+    display: none;
+  }
+
+  summary::after {
+    content: '[+]';
+    color: var(--text-color-muted);
+  }
+
+  details[open] > summary::after {
+    content: '[-]';
+  }
+
+  summary:hover {
+    background: var(--background-muted);
+    border-radius: 2px;
   }
 }
 </style>
