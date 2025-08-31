66c45f9ffdee33f757e15057c54023a45f5fb714
Milorad Filipovic <milorad.filipovic19@gmail.com>
2025-08-31 13:51:38 +0200

    📚 Updated home page

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import { computed } from 'vue'
 import InputPane from '@/components/input/InputPane.vue'
 import OutputPane from '@/components/output/OutputPane.vue'
 import { useHighlightTheme } from '@/composables/useHighlightTheme'
+import { Eye } from 'lucide-vue-next'
 
 const uiStore = useUIStore()
 
@@ -21,15 +22,37 @@ useHighlightTheme()
     </div>
     <div :class="$style.content">
       <header :class="$style.header">
-        <h1>
-          <code>Interactive ASCII Tree generator</code>
-        </h1>
+        <h1>Collapsible ASCII Tree generator</h1>
         <p>Build interactive, HTML-only, ASCII trees from your text input.</p>
       </header>
       <div :class="$style.panes">
         <div :class="[$style.pane]"><InputPane /></div>
         <div :class="[$style.pane]"><OutputPane /></div>
       </div>
+      <section>
+        <h2>How does this work?</h2>
+        <p>
+          Collapse/expand functionality of ASCII trees is achieved using HTML <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/details" target="_blank">details</a>
+          and <a href="https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/summary" target="_blank">summary</a> HTML elements. Tree-like indentation is created using unordered lists and css.
+          ASCII-art lines are appended to list elements using the <code>::before</code> and <code>::after</code> pseudo-elements, 
+          while <a href="https://github.com/MiloradFilipovic/interactive-ascii-tree-generator/blob/a687ffbaed4420f20b1e4e6ab1f65efdb255e8ea/src/components/output/ascii/ASCIIPreview.vue#L55" target="_blank">this little hack</a> 
+          is used to render vertical lines correctly (It won't work for very nested trees but should cover most cases).
+        </p>
+      </section>
+      <section>
+        <h2>How do I use the tree I generated?</h2>
+        <p>Copy generated HTML <b>and</b> CSS from their respective tabs in the <i>Result</i> section and paste it into your project.</p>
+      </section>
+      <section>
+        <h2>Need text-only version?</h2>
+        <p>Just click the <i>Copy</i> button in the [<span :class="$style.tab"><Eye :size="12" /> Preview</span>] tab.</p>
+      </section>
+      <section>
+        <p>For bug submissions, feature requests or any other inquiries, visit the <a href="https://github.com/MiloradFilipovic/interactive-ascii-tree-generator/issues" target="_blank">GitHub Issues</a> page.</p>
+      </section>
+      <footer>
+        Created by [<a href="https://github.com/MiloradFilipovic" target="_blank">this guy</a>]
+      </footer>
     </div>
   </div>
 </template>
@@ -64,6 +87,21 @@ useHighlightTheme()
   @media screen and (max-width: 1300px) {
     padding: 0 1rem;
   }
+}
+
+a {
+  color: var(--text-color-lighter);
+}
+
+code {
+  background-color: var(--background-muted);
+  padding: 2px;
+}
+
+.tab {
+  background-color: var(--background-muted);
+  padding: 0 0.3rem;
+  font-family: inherit;
 }
 
 .header {
@@ -105,5 +143,11 @@ useHighlightTheme()
       box-sizing: border-box;
     }
   }
+}
+
+footer {
+  border-top: 1px solid var(--text-color);
+  padding: 0.3rem 0.2rem;
+  text-align: end;
 }
 </style>
